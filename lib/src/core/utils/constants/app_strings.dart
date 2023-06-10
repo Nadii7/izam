08a55533401d2130623aadafe6ece6f387f107a7
Nadii7 <m7md.nadii@gmail.com>
2023-06-11 01:51:41 +0300

    commit

--- lib/src/core/utils/constants/app_strings.dart
+++ lib/src/core/utils/constants/app_strings.dart
@@ -1,18 +1,16 @@
 class AppStrings {
-  static const String appName = 'Izam';
+  static const String done = 'Done';
   static const String email = 'Email';
+  static const String appName = 'Izam';
   static const String login = 'Log in';
   static const String password = 'Password';
+  static const String welcome = 'Welcome, Log In';
+  static const String loginFails = 'Login Failed!';
   static const String emailHint = 'WWW.DDDD@izam.co';
   static const String resetPassword = 'Reset Password';
+  static const String loginSuccess = 'Login Successful!';
   static const String forgotPassword = 'Forgot Password?';
+  static const String resetSuccess = 'Password Changed Successfully';
   static const String forgotPassDescription =
       "oops you forgot your password! don't worry enter your email and we will generate a new one for you ";
-  static const String loginFails = 'Login Failed!';
-  static const String loginSuccess = 'Login Successful!';
-  static const String resetSuccess = 'Password Changed Successfully';
-  static const String done = 'Done';
-  static const String welcome = 'Welcome, Log In';
-  // static const String englishCode = 'en';
-  // static const String englishCode = 'en';
 }
--- lib/src/core/utils/constants/constants.dart
+++ lib/src/core/utils/constants/constants.dart
@@ -12,17 +12,9 @@ const primaryBackground = Color(0xFFF4F4F6);
 const secondaryBackground = Color(0xFFFFFFFF);
 
 const error = Color(0xFFE65454);
-const success = Color(0xFF02CA79);
-const warning = Color(0xFFC96F46);
 
 var defaultPadding = EdgeInsets.symmetric(
   vertical: 10.h,
   horizontal: 10.w,
 );
 
-var defaultPadding2 = EdgeInsets.symmetric(
-  vertical: 5.h,
-  horizontal: 5.w,
-);
-
-double defaultHeight = kMinInteractiveDimensionCupertino;
